ea2c9ccb397f42a827a410f9b24e6e3fb21b12b7
SujitHirave <hiravesujit11@gmail.com>
2021-07-31 00:18:30 +0530

    commit

--- app _qr _scan.py
+++ app _qr _scan.py
@@ -2,6 +2,7 @@
 from pyzbar.pyzbar import decode
 from PIL import Image
 
+# 'qrcode_Final_4.png' ticket image want to scan
 d=decode(Image.open('qrcode_Final_4.png'))
 # d=decode(Image.open('qrcode_test_2.png'))
 # d=decode(Image.open('qrcode_face_3.png'))
